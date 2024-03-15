50676bbe3367dbab2074afc7e4c6d055a03a9eff
munjieun <mjsu10@naver.com>
2024-03-15 11:06:58 +0900

    initial commit

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,243 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+
+* {
+  -webkit-text-size-adjust: none;
+  box-sizing: border-box;
+}
+
+/* 모든 Device를 위한 공통 로드 */
+body,
+div,
+dl,
+dt,
+dd,
+ul,
+ol,
+li,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+pre,
+code,
+form,
+fieldset,
+legend,
+input,
+textarea,
+p,
+blockquote,
+th,
+td,
+figure,
+figcaption,
+span {
+  margin: 0;
+  padding: 0;
+  letter-spacing: -0.5px;
+}
+
+body,
+html,
+div,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+ul,
+ol,
+dl,
+dt,
+dd,
+p hr,
+button,
+input,
+form,
+fieldset,
+table,
+caption,
+th,
+td,
+header,
+nav,
+section,
+article,
+aside,
+footer,
+textarea {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  width: auto;
+  height: 100%;
+  font-family: 'PP Neue Montreal','Pretendard', sans-serif;
+  font-size: 10px;
+  color: #0d0d0d;
+
+}
+
+header,
+nav,
+section,
+article,
+aside,
+footer {
+  display: block;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: inherit;
+  font-weight: inherit;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+legend,
+caption {
+  text-indent: -100%;
+  text-indent: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 0;
+  line-height: 0;
+  text-shadow: none;
+  color: transparent;
+}
+
+img {
+  max-width: 100%;
+  border: 0;
+  vertical-align: top;
+}
+
+table {
+  border-spacing: 0;
+  border-collapse: collapse;
+}
+
+table,
+th,
+td {
+  border: none;
+  text-align: left;
+}
+
+th,
+label {
+  font-weight: normal;
+}
+
+address {
+  font-style: normal;
+}
+
+button,
+input {
+  border: none;
+  background: none;
+}
+
+button:focus,
+input:focus {
+  outline: none;
+}
+
+button,
+a,
+input,
+label {
+  cursor: pointer;
+  text-decoration: none;
+  outline: 0;
+  font-family: inherit;
+  color: #000;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+input[type='checkbox']:disabled,
+input[type='radio']:disabled,
+button:disabled {
+  cursor: no-drop;
+}
+
+input[type='checkbox']:disabled+label,
+input[type='radio']:disabled+label {
+  cursor: no-drop;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+}
+
+input::-ms-clear {
+  display: none;
+}
+
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+textarea {
+  resize: none;
+}
+
+textarea:placeholder {
+  color: #A2A2A2;
+}
+
+textarea:focus {
+  outline: none;
+}
+
+/* select*/
+select {
+  border-radius: 0;
+  /* 아이폰 사파리 보더 없애기 */
+  -webkit-appearance: none;
+  /* 화살표 없애기 for chrome*/
+  -moz-appearance: none;
+  /* 화살표 없애기 for firefox*/
+  appearance: none;
+  /* 화살표 없애기 공통*/
+}
+
+select::-ms-expand {
+  display: none;
+  /* 화살표 없애기 for IE10, 11*/
+}
+
+
+
+
+`
+
+export default GlobalStyles;
---
+++ src/styles/Theme.js
@@ -0,0 +1,22 @@
+const Theme = {
+  colors:{
+    exLightGrey:"#F2F2F2",
+    lightGrey:"#BFBFBF",
+    mediumGrey:"#8C8C8C",
+    grey:"#404040",
+    black:"#0D0D0D",
+    purple:"#8C2A8C",
+    green:"318C2A",
+    red:"A6341B"
+  },
+  fontSize:{
+    h1:"64px",
+    h2:"48px",
+    h3:"36px",
+    h4:"24px",
+    body:"16px",
+    small:"14px",
+  }
+}
+
+export default Theme;
